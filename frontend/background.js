4e65c0d3d86f4f3ad2e27a45029d560a43f8599c
caton <bcaton85@gmail.com>
2017-11-20 10:11:12 -0500

    Restructured backend

--- frontend/background.js
+++ frontend/background.js
@@ -1,13 +1,8 @@
-/*
- *Sends message to conent script when the icon is clicked on.
- */
+//Recieved button click, send message to notify content script
 chrome.browserAction.onClicked.addListener(function(tab) {
-
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-
   });
 });
 
@@ -15,27 +10,27 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
 
-    if( request.message === "retrieved_url" ) {
-    	
-    	var params = request.url;
+    //Send post request to python server with the url
+    if( request.message === "retrieved_url" ) { 	
+
+      var params = {url: request.url};
     	var http = new XMLHttpRequest();
     	http.open("POST", "http://localhost:8080", true);
+		  http.setRequestHeader("Content-type", "application/json");
 
-		http.setRequestHeader("Content-type", "text/plain");
-
-		http.onreadystatechange = function() {
+      //Recieve the the parsed article back from the server and send it to the content script
+		  http.onreadystatechange = function() {
 			if(http.readyState == 4 && http.status == 200) {
 
 				chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-
         var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"message": "summary_returned","summary":http.responseText});
+        chrome.tabs.sendMessage(activeTab.id, {"message": "summary_returned","summary":JSON.parse(http.responseText)});
 
         });
 		  }
     }
-
-		http.send(params);
+    
+    http.send(JSON.stringify(params));
     
   }
 });
