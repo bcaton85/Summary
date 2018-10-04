7be52ccca45c0526919522797362807f1e56966a
caton <bcaton85@gmail.com>
2018-10-04 13:01:01 -0400

    should make conflict

--- backend/summarize.py
+++ backend/summarize.py
@@ -5,6 +5,7 @@ client = textapi.Client("username", "password")
 
 def summarizeArticle(url):
 	summary = ""
+	print("confflict")
 	summaryResponse = client.Summarize({'url': url, 'sentences_number': 4})
 	for sentence in summaryResponse['sentences']:
   		summary += "--" + sentence +"\n\n"
