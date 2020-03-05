0ad0c98a9a637d403348d4805a6e3f922702c710
Brandon Caton <bcaton85@gmail.com>
2020-03-04 20:16:23 -0500

    removed sentiment portion

--- backend/summarize.py
+++ backend/summarize.py
@@ -1,11 +1,11 @@
 from analyze import analyze
 from aylienapiclient import textapi
 
-client = textapi.Client("6d860f5f", "951d4533c5d65f1d97ea52d95e468cc2")
+client = textapi.Client("", "")
 
 def summarizeArticle(url):
 	summary = ""
 	summaryResponse = client.Summarize({'url': url, 'sentences_number': 4})
 	for sentence in summaryResponse['sentences']:
 		summary += "--" + sentence +"\n\n"
-	return analyze(summary)
+	return summary
